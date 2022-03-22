7568defc6a910ffe63af13e94e8c5d98c1713793
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2022-03-22 14:09:38 +0800

    ind

--- index.php
+++ index.php
@@ -4,29 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Photo Sharing App</title>
-    <link rel="stylesheet" type="text/CSS" href="index.css">
-    <header>
-        <nav>
-            <ul class="nav_links">
-                <li><a href="./lobby.html"> Home</a></li>
-                <li><a href="./login.html">Log In</a></li>
-            </ul>
-        </nav>
-    </header>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/CSS" href="lobby.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
-    <form action="./formHandlerSign.php" method="POST">
-        <h1>Sign Up</h1>
-        <input type="text" name="Username" placeholder="Username" required>
-        <select name="gender" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="pwd" placeholder="Password" required>
-        <button type="submit">Sign Up</button>
-    </form>
+   <section>
+    <header>
+      <a href="#" ><img src="paslogo.png" class="logo"></a>
+      <ul>
+          <li><a href="./lobby.html">Home</a></li>
+          <li><a href="./newSignUp.html">Sign Up</a></li>
+          <li><a href="./newLogIn.html">Log In</a></li>
+      </ul>
+    </header>
+        <div class="content">
+            <div class="textBox">
+                <h2>  <span>Photo Sharing App</span></h2>
+                <p>This is an app that contains similar features to Instagram. <br> It give people the power to <span>build community </span>and bring the world closer together.
+                <br> This app is committed to fostering a <span>safe </span>and <span>supportive community </span>for everyone.</p>
+                <button id="learnmore">Learn More</button>
+                <div id="myModal" class="modal">
+                    <div class="modal-content">
+                      <span class="close">&times;</span>
+                      <p>Sign Up or Log In to learn more about this app</p>
+                      <a href="./newSignUp.html">Sign Up</a>
+                      <a href="./newLogIn.html">Log In</a>
+                    </div>
+                  
+                </div>
+                <script>  
+                    var modal = document.getElementById("myModal");
+                    var btn = document.getElementById("learnmore");
+                    var span = document.getElementsByClassName("close")[0];          
+                    btn.onclick = function() {
+                      modal.style.display = "block";
+                    }
+                    span.onclick = function() {
+                      modal.style.display = "none";
+                    }
+                    window.onclick = function(event) {
+                      if (event.target == modal) {
+                        modal.style.display = "none";
+                      }
+                    }
+                    </script>
+            </div>
+            <div class="imgBox">
+                <img src="sharingAppLogo.png">
+            </div>
+        </div>
+   </section>
 </body>
 </html>
